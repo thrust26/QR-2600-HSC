f5f78deff115ebbdd3e1547e99a1c10bd78af004
thrust26 <tjentzsch@yahoo.de>
2026-09-23 17:54:29 +0200

    mini fix

--- QR_Code.asm
+++ QR_Code.asm
@@ -22,7 +22,7 @@ QR_SPRITE_GFX   = 1 ; (-53 bytes) display playfield(0) or sprite graphics(1)
 ; Q R   M A C R O S
 ;===============================================================================
 
-  include QRCodeGen2600_B.inc ; contains all QR code generation code
+  include QRCodeGen2600.inc ; contains all QR code generation code
 
 
 ;===============================================================================
